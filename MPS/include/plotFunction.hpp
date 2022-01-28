3d69b446c8094607eeac7e230359dfa5d40b1d76
AquaCoder0010 <93863375+AquaCoder0010@users.noreply.github.com>
2022-01-28 23:00:06 +0300

    Update plotFunction.hpp

--- MPS/include/plotFunction.hpp
+++ MPS/include/plotFunction.hpp
@@ -5,13 +5,12 @@
 
 class Plot{
 public:
-	Plot();
-	void drawParabola(unsigned int m_x, unsigned int m_y);
+    Plot();
+    void drawParabola(unsigned int m_x, unsigned int m_y);
     void drawSin(unsigned int m_x, unsigned int m_y);
     void drawLog(unsigned int m_x, unsigned int m_y);
     void drawExp(unsigned int m_x, unsigned int m_y);
-	sf::VertexArray m_Function;
-    float t;
+    sf::VertexArray m_Function;
 private:
 	float x, y;
     
